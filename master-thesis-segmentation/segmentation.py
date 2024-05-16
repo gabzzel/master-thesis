f5e412e46aff96a7d5edf71b4212e8dc14da6db6
Gabriël <45881481+gabzzel@users.noreply.github.com>
2024-05-16 15:47:33 +0200

    Improved HDBSCAN

--- master-thesis-segmentation/segmentation.py
+++ master-thesis-segmentation/segmentation.py
@@ -1,3 +1,4 @@
+import math
 import sys
 import time
 from os import PathLike
@@ -95,6 +96,9 @@ def hdbscan(points: np.ndarray,
         print("Clustering done.")
         print(f"Created {number_of_clusters} clusters in {round(end_time - start_time, 4)} seconds.")
 
+    if number_of_clusters == 0:
+        return
+
     cluster_sizes = []
     for i in range(number_of_clusters):
         cluster = np.count_nonzero(cluster_per_point == i)
@@ -110,7 +114,7 @@ def hdbscan(points: np.ndarray,
 
     config.noise_indices = np.nonzero(cluster_per_point < 0)[0]
 
-    if cluster_per_point is not None and config.noise_nearest_neighbours > 0:
+    if cluster_per_point is not None and config.noise_nearest_neighbours > 0 and np.count_nonzero(cluster_per_point < 0) > 0:
         new_clusters_for_noise = assign_noise_nearest_neighbour_cluster(points, cluster_per_point,
                                                                         config.noise_nearest_neighbours)
         cluster_per_point[cluster_per_point < 0] = new_clusters_for_noise
@@ -352,28 +356,54 @@ def assign_noise_nearest_neighbour_cluster(points: np.ndarray,
     kd_tree = KDTree(data_points)
 
     if neighbour_count == 1:
-        _, nearest_neighbour_indices = kd_tree.query(noise_points, k=1, workers=-1)
-        new_clusters = cluster_per_point[data_points_indices[nearest_neighbour_indices]]
+        return get_noise_clusters_k1(cluster_per_point, data_points_indices, kd_tree, noise_points)
     else:
-        _, nearest_neighbour_indices = kd_tree.query(noise_points, k=neighbour_count, workers=-1)
-        neighbouring_clusters = cluster_per_point[data_points_indices[nearest_neighbour_indices]]
-        u, indices = np.unique(neighbouring_clusters, return_inverse=True)
-        axis = 1
-        try:
-            x = np.apply_along_axis(np.bincount, axis, indices.reshape(neighbouring_clusters.shape),None, np.max(indices) + 1)
-            arg_max = np.argmax(x, axis=axis)
-            new_clusters = u[arg_max]
-        except Exception as e:
-            print(f"Got exception, assigning noise points in the naive way; {e}")
-            new_clusters = np.full(shape=(noise_points.shape[0]), fill_value=-1)
-            for i in range(len(noise_points)):
-                nearest_neighbours = nearest_neighbour_indices[i]
-                nearest_neighbour_clusters = cluster_per_point[data_points_indices[nearest_neighbours]]
-                counts = [np.count_nonzero(nearest_neighbour_clusters == i) for i in range(len(np.unique(cluster_per_point)))]
-                argmax = np.argmax(counts)
-                new_clusters[i] = argmax
+        return get_noise_clusters_kx(cluster_per_point, data_points_indices, kd_tree, neighbour_count,
+                                     noise_point_indices, noise_points)
 
 
+def get_noise_clusters_kx(cluster_per_point, data_points_indices, kd_tree, neighbour_count, noise_point_indices,
+                          noise_points):
+    _, nearest_neighbour_indices = kd_tree.query(noise_points, k=neighbour_count, workers=-1)
+    neighbouring_clusters = cluster_per_point[data_points_indices[nearest_neighbour_indices]]
+
+    try:
+        arg_max, u = get_noise_clusters_kx_unsafe(neighbouring_clusters)
+        return u[arg_max]
+    except Exception as e:
+        print(f"Got exception, assigning noise points in the naive way; {e}")
+        return _get_noise_clusters_kx_batches(cluster_per_point, data_points_indices, nearest_neighbour_indices,
+                                              noise_point_indices, noise_points)
+
+
+def get_noise_clusters_kx_unsafe(neighbouring_clusters):
+    u, indices = np.unique(neighbouring_clusters, return_inverse=True)
+    axis = 1
+    # noinspection PyTypeChecker
+    x = np.apply_along_axis(np.bincount, axis, indices.reshape(neighbouring_clusters.shape), None,
+                            np.max(indices) + 1)
+    arg_max = np.argmax(x, axis=axis)
+    return arg_max, u
+
+
+def _get_noise_clusters_kx_batches(cluster_per_point, data_points_indices, nearest_neighbour_indices,
+                                   noise_point_indices, noise_points):
+    new_clusters = np.full(shape=(noise_points.shape[0]), fill_value=-1)
+    batch_size = 100
+    number_of_batches = math.ceil(len(noise_points) / batch_size)
+    for i in tqdm.trange(number_of_batches, desc="Assigning noise points to cluster (batches)"):
+        # batch = noise_points[i * batch_size: (i + 1) * batch_size]
+        batch_indices = noise_point_indices[i * batch_size:(i + 1) * batch_size]
+        nearest_neighbours = nearest_neighbour_indices[batch_indices]
+        nearest_neighbour_clusters = cluster_per_point[data_points_indices[nearest_neighbours]]
+        bins = np.apply_along_axis(np.bincount, axis=1, arr=nearest_neighbour_clusters)
+        new_clusters[batch_indices] = np.argmax(bins, axis=1)
+    return new_clusters
+
+
+def get_noise_clusters_k1(cluster_per_point, data_points_indices, kd_tree, noise_points):
+    _, nearest_neighbour_indices = kd_tree.query(noise_points, k=1, workers=-1)
+    new_clusters = cluster_per_point[data_points_indices[nearest_neighbour_indices]]
     return new_clusters
 
 
